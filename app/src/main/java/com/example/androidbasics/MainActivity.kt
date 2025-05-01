d4edf71a4ff73f306de734830e7689a8eb2c81ee
AlexTamayoC <tamayoc.alejandro@hotmail.com>
2025-05-01 17:38:23 -0500

    Lo mas basico

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -1,47 +1,39 @@
-package com.example.androidbasics
+package com.example.androidbasics.com.example.androidbasics
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidbasics.ui.theme.AndroidBasicsTheme
+fun main() {
+    piNumber()
+    imprimirMensaje()
+    probarBool(true)
+    pedirDatos()
+}
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AndroidBasicsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+fun piNumber() {
+    val pi = 3.14159265359
+    println(pi)
+}
+
+fun imprimirMensaje() {
+    val mensaje = "Hola"
+    println(mensaje)
+}
+
+fun probarBool(choice: Boolean){
+    if (choice){
+        println("Yes")
+    } else{
+        println("No")
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hi $name!",
-        modifier = modifier
-    )
-}
+fun pedirDatos(){
+    println("Dame tu nombre")
+    val nombre = readLine()
+    println("Dame tu edad")
+    val edad = readLine()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidBasicsTheme {
-        Greeting("Android")
-    }
+    println("""
+        ***Datos***
+        Nombre: $nombre
+        Edad: $edad años
+    """.trimIndent())
 }
