62983b98844c2fc5ac7e305af9f2e9c2271f4214
AlexTamayoC <tamayoc.alejandro@hotmail.com>
2025-05-02 11:53:59 -0500

    Operaciones aritmeticas y funciones basicas

--- app/src/main/java/com/example/androidbasics/OperacionesAritmeticas.kt
+++ app/src/main/java/com/example/androidbasics/OperacionesAritmeticas.kt
@@ -1,2 +1,12 @@
 package com.example.androidbasics
 
+fun main() {
+    opResiduo()
+}
+
+fun opResiduo() {
+    var numero = 10
+    numero += 5
+    var resultado = numero % 5
+    print(resultado)
+}
